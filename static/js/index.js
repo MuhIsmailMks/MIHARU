917ecc88bc19798005a6d1f2f78f826d2808bda3
muhammad ismail <muhismailmks@gmail.com>
2024-08-24 12:24:09 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -14,23 +14,23 @@ menu_btn.addEventListener('click', () => {
  
 
 // copy address
-// const copyAddress = document.querySelector('.copy-box');
+const copyAddress = document.querySelector('.copy-box');
     
-// let text = document.querySelector('.copy-box__text');
-// let btn = document.querySelector('.copy-box__btn');
-// let btnText = btn.textContent;
-// let timeout;
+let text = document.querySelector('.copy-box__text');
+let btn = document.querySelector('.copy-box__btn');
+let btnText = btn.textContent;
+let timeout;
 
-// copyAddress.addEventListener('click', () => { 
-//     navigator.clipboard.writeText(text.textContent).then(function () {
-//         btn.textContent = 'Copied';
+copyAddress.addEventListener('click', () => { 
+    navigator.clipboard.writeText(text.textContent).then(function () {
+        btn.textContent = 'Copied';
 
-//         clearTimeout(timeout);
-//         timeout = setTimeout(function () {
-//             btn.textContent = btnText;
-//         }, 2000);
-//     }).catch(function (err) {
-//         console.error('Failed to copy text: ', err);
-//     });
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            btn.textContent = btnText;
+        }, 2000);
+    }).catch(function (err) {
+        console.error('Failed to copy text: ', err);
+    });
     
-// })
+})
