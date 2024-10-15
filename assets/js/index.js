1665001a7c55e2b94d63535ae631bc5eb8c55cf8
muhammad ismail <muhismailmks@gmail.com>
2024-10-15 08:45:19 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-}, false);
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+// }, false);
 
-document.onkeydown = function(e) {
-    if (e.keyCode == 123 || // F12 key
-        (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // CTRL+SHIFT+I
-        (e.ctrlKey && e.shiftKey && e.keyCode == 74)) { // CTRL+SHIFT+J
-        return false;
-    }
-};
+// document.onkeydown = function(e) {
+//     if (e.keyCode == 123 || // F12 key
+//         (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // CTRL+SHIFT+I
+//         (e.ctrlKey && e.shiftKey && e.keyCode == 74)) { // CTRL+SHIFT+J
+//         return false;
+//     }
+// };
 
 
 // choise button
