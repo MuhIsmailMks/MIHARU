67d48690841661b8d36cc6b3f20c3513702b38e8
muhammad ismail <muhismailmks@gmail.com>
2024-10-15 14:56:16 +0800

    update

---
+++ 'data_image/png;base64,'.html
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Not found</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body
+    style="font-family: Open Sans,Helvetica Neue,Helvetica,Arial,sans-serif; margin: 0; padding: 0;">
+<div
+    style="height: 100vh; display: flex; align-items: center;">
+  <div
+      style="margin: 0 auto; text-align: center; max-width: 480px; max-height: 480px; padding: 16px">
+    <h1>Not found (404)</h1>
+    <p>That means the page you’re looking for doesn’t exist.</p>
+  </div>
+</div>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,42 @@
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+// }, false);
+
+// document.onkeydown = function(e) {
+//     if (e.keyCode == 123 || // F12 key
+//         (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // CTRL+SHIFT+I
+//         (e.ctrlKey && e.shiftKey && e.keyCode == 74)) { // CTRL+SHIFT+J
+//         return false;
+//     }
+// };
+
+
+// choise button
+ // Seleksi semua tombol di dalam container
+ const buttons = document.querySelectorAll('.choiseBtnContainer button');
+ // Seleksi semua elemen dengan id yang sama dengan data-btn
+ const contentElements = document.querySelectorAll('.choiseMemes');
+
+ buttons.forEach(button => {
+     button.addEventListener('click', () => {
+         // Hapus class 'active' dari semua tombol
+         buttons.forEach(btn => btn.classList.remove('active'));
+         // Tambahkan class 'active' ke tombol yang diklik
+         button.classList.add('active');
+
+         // Dapatkan nilai data-btn dari tombol yang diklik
+         const btnData = button.getAttribute('data-btn');
+
+         // Loop untuk menghilangkan class 'active' dari semua content
+         contentElements.forEach(content => {
+             // Cek apakah id content sama dengan data-btn
+             if (content.id === btnData) {
+                 content.classList.add('active');
+             } else {
+                 content.classList.remove('active');
+             }
+         });
+     });
+ });
+
+ 
